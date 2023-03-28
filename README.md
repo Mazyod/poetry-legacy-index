76f4561fb1e296892a7683ea8fa1991d10337216
Maz Jaleel <mazjaleel@gmail.com>
2023-03-28 09:55:59 +0300

    implement patching

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ This project is a poetry plugin that has one simple purpose:
 
 Long story short, TensorFlow 2.11.x includes different metadata per platform wheel, and poetry simply downloads the first wheel found in a legacy index (e.g. devpi, pypiserver), which causes it to incorrectly resolve the dependencies.
 
-Another nice side effect is that (based on a discussion on the poetry repo) poetry doesn't cache the downloaded wheels at the "solving" phase, which means that if the first wheel downloaded is not the one that will be installed, it will be downloaded again and again each time (~500 MB for TensorFlow). By downloading the wheel for the given platform, it will *at least* check the cache before attempting to download the wheel, and the wheel should be available if it was already downloaded for installation.
+Another nice side effect is that ([based on a discussion on the poetry repo][poetry-issue-4000]) poetry doesn't cache the downloaded wheels at the "solving" phase, which means that if the first wheel downloaded is not the one that will be installed, it will be downloaded again and again each time (~500 MB for TensorFlow). By downloading the wheel for the given platform, it will *at least* check the cache before attempting to download the wheel, and the wheel should be available if it was already downloaded for installation.
+
+
+[poetry-issue-4000]: https://github.com/python-poetry/poetry/issues/4000
--- src/poetry_legacy_index/main.py
+++ src/poetry_legacy_index/main.py
@@ -16,7 +16,37 @@ class LegacyIndexFixCommand(GroupCommand):
     options = []
 
     def handle(self) -> int:
-        print("Running legacy index fix command.")
+        import importlib.util
+        import inspect
+        from pathlib import Path
+
+        # get the path to the poetry package
+        repo_spec = importlib.util.find_spec("poetry.repositories.http_repository")
+        patch_file_path = Path(repo_spec.origin).resolve()
+
+        # prepare the file content
+        with patch_file_path.open("r") as f:
+            content = f.read()
+
+        # check if the file is already patched
+        if FILTER_WHEELS_CALL in content:
+            self.line("Legacy repository patch already applied.", "debug")
+            return
+
+        # append the filter_wheels function
+        content += "\n\n"
+        content += inspect.getsource(filter_wheels)
+
+        # replace the function call
+        content = content.replace(FILTER_WHEELS_CALL_TO_REPLACE, FILTER_WHEELS_CALL)
+
+        # write the patched file
+        with patch_file_path.open("w") as f:
+            f.write(content)
+
+        self.line(f"Successfully patched {patch_file_path}", "success")
+        self.line_error("Please clear the poetry cache and try again.", "warning")
+
         return 0
 
 
@@ -33,6 +63,38 @@ class ExportApplicatioLegacyIndexFixPlugin(ApplicationPlugin):
         # for your own plugins: DON'T DO THIS!
         command = LegacyIndexFixCommand()
         command.set_application(application)
-        command.handle()
+        command.execute(io=application._io)
 
         super().activate(application=application)
+
+
+# filter wheels func to be appended at the end of the file
+def filter_wheels(wheels):
+    """Filter out wheels that are not compatible with the current platform."""
+    import sys
+    from pathlib import Path
+
+    from poetry.utils.env import SystemEnv
+
+    env = SystemEnv(Path(sys.executable))
+    markers = env.get_marker_env()
+    filtered_wheels = wheels
+    platform_wheels = [
+        w for w in filtered_wheels if markers.get("sys_platform", "") in w
+    ]
+    if platform_wheels:
+        filtered_wheels = platform_wheels
+    machine_wheels = [
+        w for w in filtered_wheels if markers.get("platform_machine", "") in w
+    ]
+    if machine_wheels:
+        filtered_wheels = machine_wheels
+
+    return filtered_wheels
+
+
+# The function call to filter the wheels
+FILTER_WHEELS_CALL = "first_wheel = filter_wheels(platform_specific_wheels)[0]"
+
+# The code to be replaced by the filter_wheels_call
+FILTER_WHEELS_CALL_TO_REPLACE = "first_wheel = platform_specific_wheels[0]"
